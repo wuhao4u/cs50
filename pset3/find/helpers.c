c82290820bab378486627e5f8d56fb29bf321e65
David Wu <wuhao4u@163.com>
2017-05-10 05:34:41 +0000

    added quicksort and binary search for find

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -24,14 +24,45 @@ bool linear_search(int value, int values[], int n) {
     return false;
 }
 
+bool binary_search(int target, int A[], int n) {
+    if (n == 0) {
+        return false;
+    }
+    
+    int start = 0;
+    int end = n - 1;
+    int mid;
+
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                end = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else if (A[mid] > target) {
+                end = mid;
+            }
+        }
+
+        if (A[start] == target) {
+            return true;
+        }
+
+        if (A[end] == target) {
+            return true;
+        }
+        
+    return false;
+}
+
 /**
  * Returns true if value is in array of n values, else false.
  */
 bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
-    return linear_search(value, values, n);
-//    return false;
+    // return linear_search(value, values, n);
+    return binary_search(value, values, n);
 }
 
 
@@ -51,6 +82,26 @@ void bubble_sort(int values[], int n) {
     }
 }
 
+void qsort_hao(int v[], int left, int right) {
+    int i, last;
+    void swap(int v[], int i, int j);
+
+    if (left >= right)
+        return;
+
+    swap(v, left, (left+right)/2);
+    last = left;
+    for (i = left+1; i <= right; i++) {
+        if (v[i] < v[left]) {
+            swap(v, ++last, i);
+        }
+    }
+
+    swap(v, left, last);
+    qsort_hao(v, left, last-1);
+    qsort_hao(v, last+1, right);
+}
+
 
 /**
  * Sorts array of n values.
@@ -58,6 +109,7 @@ void bubble_sort(int values[], int n) {
 void sort(int values[], int n)
 {
     // TODO: implement a sorting algorithm
-    bubble_sort(values, n);
+    // bubble_sort(values, n);
+    qsort_hao(values, 0, n);
     return;
 }
